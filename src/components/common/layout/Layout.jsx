ce4d7a2980b4f0e1d1a26c07c4b3ad40a97c31f8
kenbugless <kenbugless@gmail.com>
2023-10-08 14:51:21 +0900

    서브페이지 텍스트 모션 처리

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -3,22 +3,26 @@ import { useEffect, useRef } from 'react';
 
 export default function Layout({ children, title }) {
 	let newClass = title.toLowerCase().split(' ').join('_');
+	const refFrame = useRef(null);
 	const refTitle = useRef(null);
 
 	useEffect(() => {
 		let text = refTitle.current.innerText;
 		let tags = '';
 		let count = 0;
-
 		for (let letter of text) {
-			tags += `<span style='display:inline-block';transition-delay:${0.1 * count}s'> ${letter} </span>`;
+			tags += `<span style='display:inline-block; transition-delay:${0.1 * count}s'>${letter}</span>`;
 			count++;
 		}
 		refTitle.current.innerHTML = tags;
+
+		setTimeout(() => {
+			refFrame.current.classList.add('on');
+		}, 300);
 	}, []);
 
 	return (
-		<section className={`layout ${newClass}`}>
+		<section ref={refFrame} className={`layout ${newClass}`}>
 			<h1 ref={refTitle}>{title}</h1>
 			{children}
 		</section>
