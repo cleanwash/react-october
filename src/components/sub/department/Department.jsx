ecb066f096284a23e2647fc8b8e14593b13f377e
kenbugless <kenbugless@gmail.com>
2023-10-14 14:01:05 +0900

    복잡한 구조의 JSON 데이터 렌더링 처리

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,45 +1,37 @@
-import Layout from '../../common/layout/Layout';
-import { useEffect, useState } from 'react';
 import './Department.scss';
+import Layout from '../../common/layout/Layout';
+import React, { useEffect, useState } from 'react';
 import { useFetch } from '../../../hooks/useFetch';
 
-/*리액트에서 외부 데이터를 fetching하는 작업 흐름
-1. 컴포넌트 함수가 호출되고 외부 데이터를 받을 state 초기화 
-2. 의존성 배열이 비어있는 useEffect hook 안쪽에서 fetch로 비동기 데이터를 받고, state에 넘겨줌 
-3. useEffect 안쪽에서 fetch문을 써야되는 이유는 fetch 자체가 web api를 통해서 client 기반으로 데이터를 전달받는 방식이기 때문에, 
-무조건 컴포넌트가 마운트 되어야지만 호출 가능 
-4. useEffect에 의해서 데이터가 받아지면, state에 전달되면 자동으로 컴포넌트는 재렌더링됨 
-5. 비동기 데이터를 활용해서 실제 JSX로 동적 DOM을 생성하는 시점은 두번 째, 랜더링 타임 
-*/
-
 const path = process.env.PUBLIC_URL;
+
 export default function Department() {
 	const [Title, setTitle] = useState('');
 	const [Department, setDepartment] = useState([]);
+	const [History, setHistory] = useState([]);
 	const fetchData = useFetch();
-	// const fetchData = (url, setValue, setKey) => {
-	// 	fetch(url)
-	// 		.then((data) => data.json())
-	// 		.then((json) => {
-	// 			setValue(Object.values(json)[0]);
-	// 			setKey && setKey(Object.keys(json)[0]);
-	// 		});
-	// };
+	console.log(History);
 
 	useEffect(() => {
+		fetchData(`${path}/DB/history.json`, setHistory);
 		fetchData(`${path}/DB/department.json`, setDepartment, setTitle);
 	}, []);
+
 	return (
 		<Layout title={'Department'}>
 			<section id='historyBox'>
-				<h2>History</h2>
-				<article>
-					<h3>2016</h3>
-					<ul>
-						<li>This company was established.</li>
-						<li>Launch First Prototype Project</li>
-					</ul>
-				</article>
+				{History.map((data, idx) => {
+					return (
+						<React.Fragment key={idx}>
+							<h2>{Object.keys(data)[0]}</h2>
+							<ul>
+								{Object.values(data)[0].map((val, idx) => (
+									<li key={idx}>{val}</li>
+								))}
+							</ul>
+						</React.Fragment>
+					);
+				})}
 			</section>
 
 			<section id='memberBox'>
