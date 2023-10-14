087e1ccea83485312a2a1ed90422639b7034a608
kenbugless <kenbugless@gmail.com>
2023-10-14 13:04:23 +0900

    useEffect, useState, fetch를 활용해서 비동기 데이터 기반으로 동적 돔 생성

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,24 +1,45 @@
 import Layout from '../../common/layout/Layout';
-import { useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import './Department.scss';
-import Modal from '../../common/modal/Modal';
 
+/*리액트에서 외부 데이터를 fetching하는 작업 흐름
+1. 컴포넌트 함수가 호출되고 외부 데이터를 받을 state 초기화 
+2. 의존성 배열이 비어있는 useEffect hook 안쪽에서 fetch로 비동기 데이터를 받고, state에 넘겨줌 
+3. useEffect 안쪽에서 fetch문을 써야되는 이유는 fetch 자체가 web api를 통해서 client 기반으로 데이터를 전달받는 방식이기 때문에, 
+무조건 컴포넌트가 마운트 되어야지만 호출 가능 
+4. useEffect에 의해서 데이터가 받아지면, state에 전달되면 자동으로 컴포넌트는 재렌더링됨 
+5. 비동기 데이터를 활용해서 실제 JSX로 동적 DOM을 생성하는 시점은 두번 째, 랜더링 타임 
+*/
+
+const path = process.env.PUBLIC_URL;
 export default function Department() {
-	const [Open, setOpen] = useState(false);
+	const [Department, setDepartment] = useState([]);
+	console.log('Department', Department); //빈 배열로 출력이 된다.
+	//data fetching
+	useEffect(() => {
+		fetch(`${path}/DB/department.json`)
+			.then((data) => data.json())
+			.then((json) => {
+				//console.log(json.members);
+				setDepartment(json.members);
+			});
+	}, []);
+
 	return (
 		<Layout title={'Department'}>
-			<button onClick={() => setOpen(!Open)}>{Open ? 'close' : 'open'}</button>
-			{/* {Open ? <Modal /> : null}
-			 */}
-			{Open && <Modal />}
+			<section id='memberBox'>
+				{Department.map((member, idx) => {
+					return (
+						<article key={idx}>
+							<div className='pic'>
+								<img src={`${path}/img/${member.pic}`} alt={member.name} />
+							</div>
+							<h2>{member.name}</h2>
+							<p>{member.position}</p>
+						</article>
+					);
+				})}
+			</section>
 		</Layout>
 	);
 }
-
-/*
-return문 바깥쪽에서는 모든 스크립트 구문을 다 활용이 가능하나, 
-JSX 구문 안쪽에서는 {}를 통해서 할 수 있는 연산 3가지 
-1. 변수치환
-2. map으로 반복처리
-3. 삼항연산자, && 연산자를 통한 분기처리 
-*/
