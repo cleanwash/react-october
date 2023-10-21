430411631e66bf8c518bd0c87162ae4f0cb912d8
kenbugless <kenbugless@gmail.com>
2023-10-21 13:06:26 +0900

    갤러리 출력

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -4,12 +4,12 @@ import { useState, useEffect } from 'react';
 
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
-
+	console.log(Pics);
 	const fetchFlicker = async () => {
 		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
 		const key = '2a1a0aebb34012a99c23e13b49175343';
 		const method_interest = 'flickr.interestingness.getList';
-		const num = 40;
+		const num = 40; //불러오는 갯수, flicker는 최대 500개까지 불러올 수 있다
 		const url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
 		const data = await fetch(url);
 		const json = await data.json();
@@ -23,7 +23,17 @@ export default function Gallery() {
 
 	return (
 		<Layout title={'Gallery'}>
-			<p>갤러리 상세페이지</p>
+			<div className='frame'>
+				{Pics.map((pic, idx) => {
+					return (
+						<article key={idx}>
+							<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} alt={pic.title} />
+							<h2>{pic.title}</h2>
+						</article>
+					);
+				})}
+				)
+			</div>
 		</Layout>
 	);
 }
