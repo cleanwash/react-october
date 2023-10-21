294d49ac9df3a43cb87f0023adcb97309643a008
kenbugless <kenbugless@gmail.com>
2023-10-21 13:50:46 +0900

    문자값 정리

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -4,21 +4,20 @@ import { useState, useEffect } from 'react';
 
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
-	console.log(Pics);
-	const fetchFlicker = async () => {
+
+	const fetchFlickr = async () => {
 		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
-		const key = '2a1a0aebb34012a99c23e13b49175343';
+		const key = process.env.REACT_APP_FLICKER_KEY;
 		const method_interest = 'flickr.interestingness.getList';
-		const num = 40; //불러오는 갯수, flicker는 최대 500개까지 불러올 수 있다
+		const num = 40;
 		const url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
 		const data = await fetch(url);
 		const json = await data.json();
 		setPics(json.photos.photo);
 	};
-	//console.log(Pics);
 
 	useEffect(() => {
-		fetchFlicker();
+		fetchFlickr();
 	}, []);
 
 	return (
@@ -27,12 +26,13 @@ export default function Gallery() {
 				{Pics.map((pic, idx) => {
 					return (
 						<article key={idx}>
-							<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} alt={pic.title} />
+							<div className='pic'>
+								<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} alt={pic.title} />
+							</div>
 							<h2>{pic.title}</h2>
 						</article>
 					);
 				})}
-				)
 			</div>
 		</Layout>
 	);
--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -6,8 +6,8 @@ export default function Youtube() {
 	const [Vids, setVids] = useState([]);
 
 	const fetchYoutube = async () => {
-		const api_key = 'AIzaSyA4fPQW6aWLzMytURErC1gX5nsK2Zq4M3I';
-		const pid = 'PLN5pyyn6C7blzMwK49t-fT9wdJ4bQgKjT';
+		const api_key = process.env.REACT_APP_YOUTUBE_KEY;
+		const pid = process.env.REACT_APP_PLAYLIST;
 		const num = 10; //10개 추가해서
 		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
 		//fetchData(baseURL, setVids);
@@ -23,10 +23,16 @@ export default function Youtube() {
 	return (
 		<Layout title={'Youtube'}>
 			{Vids.map((data, idx) => {
+				const title = data.snippet.title;
+				const desc = data.snippet.description;
+				const [date, time] = data.snippet.publishedAt.split('T');
+
 				return (
 					<article key={idx}>
-						<h2>{data.snippet.title}</h2>
-						<p>{data.snippet.description}</p>
+						<h2>{title}</h2>
+						<p>{desc.length > 200 ? desc.substr(0, 200) + '...' : desc}</p>
+						<span>{date.split('_').join('.')}</span>
+						<em>{time.split('Z')[0]}</em>
 						<div className='pic'>
 							<Link to={`/detail/${data.id}`}>
 								<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
