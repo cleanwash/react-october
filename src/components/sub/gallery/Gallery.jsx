29f41d5144d9c6fdf290abecd6c19338b040bdf1
kenbugless <kenbugless@gmail.com>
2023-10-21 15:25:19 +0900

    썸네일의 alt속성에 큰 이미지 url 적용

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -7,7 +7,7 @@ export default function Gallery() {
 
 	const fetchFlickr = async () => {
 		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
-		const key = process.env.REACT_APP_FLICKER_KEY;
+		const key = process.env.REACT_APP_FLICKR_KEY;
 		const method_interest = 'flickr.interestingness.getList';
 		const num = 40;
 		const url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
@@ -30,6 +30,11 @@ export default function Gallery() {
 								<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} alt={pic.title} />
 							</div>
 							<h2>{pic.title}</h2>
+
+							<div className='profile'>
+								<img src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`} alt={pic.owner} />
+								<span>{pic.owner}</span>
+							</div>
 						</article>
 					);
 				})}
