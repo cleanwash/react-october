d9dfbee68577ff4ddf8f5dbe809fe4d2e043342a
kenbugless <kenbugless@gmail.com>
2023-10-22 14:34:59 +0900

    활성화 버튼 재 클릭시 중복 핸들러호출 방지

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,37 +1,65 @@
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
 import Masonry from 'react-masonry-component';
-import { useState, useEffect } from 'react';
-
-//리액트 컴포넌트에 masonry ui 적용 방법
-//1. npm i react-masonry-component 설치
-//2. 기존의 레이아웃을 float형태로 배치(사이간격을 주고 싶을때는 padding값 활용)
-//3. 각 카드 컴포넌트를 import된 Masonry 컴포넌트로 wrapping
-//4. Masonry 컴포넌트 옵션값설정(아래 코드 참조)
+import { useState, useEffect, useRef } from 'react';
 
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
+	const refElBtnSet = useRef(null);
+	const myID = '199378814@N03';
 
-	const fetchFlickr = async () => {
+	const fetchFlickr = async (opt) => {
+		console.log('fetching again...');
 		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
-		const key = process.env.REACT_APP_FLICKER_KEY;
-		const myID = '199378814@N03';
+		const key = process.env.REACT_APP_FLICKR_KEY;
 		const method_interest = 'flickr.interestingness.getList';
 		const method_user = 'flickr.people.getPhotos';
 		const num = 40;
+		let url = '';
 		const url_interest = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
-		const url_user = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${myID}`;
-		const data = await fetch(url_user);
+		const url_user = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${opt.id}`;
+
+		opt.type === 'user' && (url = url_user);
+		opt.type === 'interest' && (url = url_interest);
+
+		const data = await fetch(url);
 		const json = await data.json();
 		setPics(json.photos.photo);
 	};
 
+	const activateBtn = (e) => {
+		const btns = refElBtnSet.current.querySelectorAll('button');
+		btns.forEach((btn) => btn.classList.remove('on'));
+		e.target.classList.add('on');
+	};
+
+	const handleClickInterest = (e) => {
+		if (e.target.classList.contains('on')) return;
+		activateBtn(e);
+		fetchFlickr({ type: 'interest' });
+	};
+
+	const handleClickMine = (e) => {
+		if (e.target.classList.contains('on')) return;
+		activateBtn(e);
+		fetchFlickr({ type: 'user', id: myID });
+	};
+
 	useEffect(() => {
-		fetchFlickr();
+		fetchFlickr({ type: 'user', id: myID });
 	}, []);
 
 	return (
 		<Layout title={'Gallery'}>
+			<article className='controls'>
+				<nav className='btnSet' ref={refElBtnSet}>
+					<button onClick={handleClickInterest}>Interest Gallery</button>
+					<button className='on' onClick={handleClickMine}>
+						My Gallery
+					</button>
+				</nav>
+			</article>
+
 			<div className='frame'>
 				<Masonry elementType={'div'} options={{ transitionDuration: '0.5s' }} disableImagesLoaded={false} updateOnEachImageLoad={false}>
 					{Pics.map((pic, idx) => {
@@ -39,7 +67,7 @@ export default function Gallery() {
 							<article key={idx}>
 								<div className='inner'>
 									<div className='pic'>
-										<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`} />
+										<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_w.jpg`} alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`} />
 									</div>
 									<h2>{pic.title}</h2>
 
@@ -47,11 +75,9 @@ export default function Gallery() {
 										<img
 											src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
 											alt={pic.owner}
-											onError={(e) => {
-												e.target.setAttribute('src', 'https://www.flicker/com/images/buddyicon.gif');
-											}}
+											onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
 										/>
-										<span>{pic.owner}</span>
+										<span onClick={(e) => fetchFlickr({ type: 'user', id: e.target.innerText })}>{pic.owner}</span>
 									</div>
 								</div>
 							</article>
