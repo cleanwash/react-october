b36d9a1e375687d6b4d91c49654159e56c17c73e
kenbugless <kenbugless@gmail.com>
2023-10-22 12:54:43 +0900

    mansory 컴포넌트 적용

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,6 +1,13 @@
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
+import Masonry from 'react-masonry-component';
 import { useState, useEffect } from 'react';
+
+//리액트 컴포넌트에 masonry ui 적용 방법
+//1. npm i react-masonry-component 설치
+//2. 기존의 레이아웃을 float형태로 배치(사이간격을 주고 싶을때는 padding값 활용)
+//3. 각 카드 컴포넌트를 import된 Masonry 컴포넌트로 wrapping
+//4. Masonry 컴포넌트 옵션값설정(아래 코드 참조)
 
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
@@ -23,21 +30,25 @@ export default function Gallery() {
 	return (
 		<Layout title={'Gallery'}>
 			<div className='frame'>
-				{Pics.map((pic, idx) => {
-					return (
-						<article key={idx}>
-							<div className='pic'>
-								<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} alt={pic.title} />
-							</div>
-							<h2>{pic.title}</h2>
+				<Masonry elementType={'div'} options={{ transitionDuration: '0.5s' }} disableImagesLoaded={false} updateOnEachImageLoad={false}>
+					{Pics.map((pic, idx) => {
+						return (
+							<article key={idx}>
+								<div className='inner'>
+									<div className='pic'>
+										<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`} />
+									</div>
+									<h2>{pic.title}</h2>
 
-							<div className='profile'>
-								<img src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`} alt={pic.owner} />
-								<span>{pic.owner}</span>
-							</div>
-						</article>
-					);
-				})}
+									<div className='profile'>
+										<img src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`} alt={pic.owner} />
+										<span>{pic.owner}</span>
+									</div>
+								</div>
+							</article>
+						);
+					})}
+				</Masonry>
 			</div>
 		</Layout>
 	);
