77e0971c801777eb6920234170b35da4fc60d02a
kenbugless <kenbugless@gmail.com>
2023-10-29 12:49:18 +0900

    community 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/common/header/Header';
 import Visual from './components/main/visual/Visual';
 import Contact from './components/sub/contact/Contact';
 import Department from './components/sub/department/Department';
+import Community from './components/sub/community/Community';
 import Gallery from './components/sub/gallery/Gallery';
 import Members from './components/sub/members/Members';
 import Youtube from './components/sub/youtube/Youtube';
@@ -18,6 +19,7 @@ function App() {
 
 			<Route exact path='/' component={Visual} />
 			<Route path='/department' component={Department} />
+			<Route path='/community' component={Community} />
 			<Route path='/gallery' component={Gallery} />
 			<Route path='/youtube' component={Youtube} />
 			<Route path='/members' component={Members} />
---
+++ src/components/sub/community/Community.jsx
@@ -0,0 +1,7 @@
+import './Community.scss';
+import Layout from '../../common/layout/Layout';
+function Community() {
+	return <Layout title={'community'}>Community</Layout>;
+}
+
+export default Community;
