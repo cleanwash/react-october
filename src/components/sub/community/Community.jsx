03840967cd289c809e3e99048260daf95a99ecee
kenbugless <kenbugless@gmail.com>
2023-10-29 14:15:22 +0900

    글저장 함수 호출해서 Posts값이 변경될때마다 통째로 문자화해서 로컬저장소에 저장

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -2,7 +2,7 @@ import './Community.scss';
 import Layout from '../../common/layout/Layout';
 import { TfiWrite } from 'react-icons/tfi';
 import { ImCancelCircle } from 'react-icons/im';
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 function Comunity() {
 	const refInput = useRef(null);
@@ -28,6 +28,10 @@ function Comunity() {
 		]);
 		resetPost();
 	};
+
+	useEffect(() => {
+		localStorage.setItem('posts', JSON.stringify(Posts));
+	}, [Posts]);
 
 	return (
 		<Layout title={'Community'}>
