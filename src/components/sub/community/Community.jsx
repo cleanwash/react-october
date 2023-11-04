e261d494945c219eb6bb64ef9208db98fa95ccc1
kenbugless <kenbugless@gmail.com>
2023-11-04 13:41:51 +0900

    수정모드 변경함수 추가 및 이벤트 연결

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -1,7 +1,7 @@
 import './Community.scss';
 import Layout from '../../common/layout/Layout';
 import { TfiWrite } from 'react-icons/tfi';
-import { ImCancelCircle } from 'react-icons/im';
+import { RxReset } from 'react-icons/rx';
 import { useRef, useState, useEffect } from 'react';
 
 function Comunity() {
@@ -13,6 +13,8 @@ function Comunity() {
 	const refInput = useRef(null);
 	const refTextarea = useRef(null);
 	const [Posts, setPosts] = useState(getLocalData());
+	console.log(Posts);
+
 	const resetPost = () => {
 		refInput.current.value = '';
 		refTextarea.current.value = '';
@@ -21,31 +23,26 @@ function Comunity() {
 	const createPost = () => {
 		if (!refInput.current.value.trim() || !refTextarea.current.value.trim()) {
 			resetPost();
-			return alert('제목값 본문을 모두 입력하세요 ');
+			return alert('제목과 본문을 모두 입력하세요.');
 		}
-		//현재 전세계 표준 시간값에서 getTIme()을 호출하면, 현재 시간값을 초단위로 밀리세컨드 단위에 숫자값으로 반환
-		//표준시값에 한국시간대가 9시간 빠르므로, 9시간에 대한 밀리세컨드 값을 더해줌 (korTime)
-		//korTime: 한국시간대를 밀리세컨드로 반환한 값
 		const korTime = new Date().getTime() + 1000 * 60 * 60 * 9;
-		//new Date(한국밀리세컨드시간값) --> 한국 시간값을 기준으로 해서 시간 객체값 반환
-		setPosts([
-			{
-				title: refInput.current.value,
-				content: refTextarea.current.value,
-				date: new Date(korTime),
-			},
-			...Posts,
-		]);
+
+		setPosts([{ title: refInput.current.value, content: refTextarea.current.value, date: new Date(korTime) }, ...Posts]);
 		resetPost();
 	};
 
 	const deletePost = (delIndex) => {
-		console.log(delIndex);
-		//Post.filter로 전달되는 삭제순번관 현재반복되는 값의 순번이 같지가 않은것만 배열로 반환(삭제순번값만 제외하고 반환하기 때문에 결과적으로 삭제와 동일한 기능 )
-		//삭제 순번글만 제외한 나머지 배열값을 다시 setPosts로 기존 Posts 값을 변경하면 컴포넌트가 재런더링 되면서 해당글만 제외한 나머지 글만 출력
-		//해당 구문에서는 filter 자체가 불변성을 유지하면서 새로운 배열을 리턴하기 때문에, 굳이 전개 연산자로 기존 state값을 deep copy할 필요가 없음
-
 		setPosts(Posts.filter((_, idx) => delIndex !== idx));
+	};
+
+	const enableUpdate = (editIndex) => {
+		setPosts(
+			//기존의 Posts배열을 반복돌면서 파라미터전달된 editIndex순번에 해다는 post객체에만 enableUpdate=true값을 추가한 객체의 배열값을 다시 기존 Posts에 변경
+			Posts.map((post, idx) => {
+				if (editIndex === idx) post.enableUpdate = true;
+				return post;
+			})
+		);
 	};
 
 	useEffect(() => {
@@ -56,12 +53,12 @@ function Comunity() {
 		<Layout title={'Community'}>
 			<div className='wrap'>
 				<div className='inputBox'>
-					<input type='text' placeholder='title' ref={refInput} />
-					<textarea cols='30' rows='3' placeholder='leave message' ref={refTextarea}></textarea>
+					<input type='text' placeholder='Write Title' ref={refInput} />
+					<textarea cols='30' rows='5' placeholder='Write Content Message' ref={refTextarea}></textarea>
 
 					<nav>
 						<button onClick={resetPost}>
-							<ImCancelCircle fontSize={20} color={'#555'} />
+							<RxReset fontSize={20} color={'#555'} />
 						</button>
 						<button onClick={createPost}>
 							<TfiWrite fontSize={20} color={'#555'} />
@@ -70,28 +67,39 @@ function Comunity() {
 				</div>
 
 				<div className='showBox'>
-					{/* 순서3 - 로컬저장소로부터 옮겨담아진 state값을 반복돌면서 글 목록 출력  */}
 					{Posts.map((post, idx) => {
-						//현재 시간값이 State에 옮겨담아지는 순간에는 객체값이고,
-						//다음번 렌더링 싸이클에서 useEffect에 의해, 문자로 변환된 다음에 로컬 저장소에 저장
-						//날짜값을 받는 첫번째 렌더링 타임에는 날짜값이 객체이므로, split 부분에서 오류 발생
-						//해결방법은 처음 렌더링을 도는 시점에서 날짜를 강제로 문자화한다음 출력처리
-						//console.log(typeof post.date);
 						const stringDate = JSON.stringify(post.date);
-						const textedDate = stringDate.split('T')[0].split('"')[1].split('_').join('.');
-						return (
-							<article key={idx}>
-								<div className='txt'>
-									<h2>{post.title}</h2>
-									<p>{post.content}</p>
-								</div>
-								<span>{textedDate} </span>
-								<nav>
-									<button>Edit</button>
-									<button onClick={() => deletePost(idx)}>Delete</button>
-								</nav>
-							</article>
-						);
+						const textedDate = stringDate.split('T')[0].split('"')[1].split('-').join('.');
+						if (post.enableUpdate) {
+							//수정모드
+							return (
+								<article key={idx}>
+									<div className='txt'>
+										<input type='text' defaultValue={post.title} />
+										<textarea defaultValue={post.content}></textarea>
+									</div>
+									<nav>
+										<button>Cancel</button>
+										<button>Update</button>
+									</nav>
+								</article>
+							);
+						} else {
+							//출력모드
+							return (
+								<article key={idx}>
+									<div className='txt'>
+										<h2>{post.title}</h2>
+										<p>{post.content}</p>
+										<span>{textedDate} </span>
+									</div>
+									<nav>
+										<button onClick={() => enableUpdate(idx)}>Edit</button>
+										<button onClick={() => deletePost(idx)}>Delete</button>
+									</nav>
+								</article>
+							);
+						}
 					})}
 				</div>
 			</div>
@@ -102,21 +110,10 @@ function Comunity() {
 export default Comunity;
 
 /*
-CRUD
-Create(글작성) "POST"
-Read(글 불러오기) "GET"
-Update(글 수정) "PUT"
-Delete(글 삭제) "DELETE"
-
-RESTful API : DB의 데이터를 구조적으로 변경하기 위한 개발방법론 
-
-로컬저장소: Local Storage
-- 모든 브라우저가 내장하고 있는 경량의 저장공간
-- 문자값만 저장 가능(5 MB)
-- 객체값을 문자화시켜서 저장
-- 로컬 저장소 값을 불러올 때는, 반대로 문자형태를 JSON 형태로 객체로 parsing해서 가져옴
-
-localStorage 메서드 
-localStorage.setItem('키','문자화 된 데이터') :로컬 저장소에 데이터 저장
-localStorage.getitem('키',) : 해당 데이터는 문자값으로 리턴되기 때문에 다시 객체 형태로 parsing 처리 필요 
+	글수정 로직 단계
+	1. 각 포스트에서 수정 버튼 클릭시 해당 객체에 enableUpdate=true라는 프로퍼티추가후 state저장
+	2. 반복돌며 렌더링시 반복도는 객체에 enableUpdate값이 true면 제목, 본문을 폼요소 출력하도록 분기처리
+	3. 수정모드일때에는 수정취소, 수정완료 버튼 생성
+	4. 수정취소버튼 클릭시 출력모드로 변경 (enableUpdat=false처리)
+	5. 수정완료버튼 클릭시 수정모드에 있는 value값을 가져와서 state에 저장한뒤 다시 출력모드로 변경처리
 */
