0773fd6517c118d26da644843feb98669b2eb7db
kenbugless <kenbugless@gmail.com>
2023-11-04 15:22:25 +0900

    메인페이지에 게시글 미리보기 출력

--- src/App.js
+++ src/App.js
@@ -6,25 +6,29 @@ import Department from './components/sub/department/Department';
 import Community from './components/sub/community/Community';
 import Gallery from './components/sub/gallery/Gallery';
 import Members from './components/sub/members/Members';
+import Detail from './components/sub/youtube/Detail';
 import Youtube from './components/sub/youtube/Youtube';
+import { useMedia } from './hooks/useMedia';
 import './styles/Global.scss';
 import { Route } from 'react-router-dom';
-import Detail from './components/sub/youtube/Detail';
-import { useMedia } from './hooks/useMedia';
+import News from './components/main/news/News';
 
 function App() {
 	return (
 		<main className={useMedia()}>
 			<Header />
-
-			<Route exact path='/' component={Visual} />
+			<Route exact path='/'>
+				<Visual />
+				<News />
+			</Route>
 			<Route path='/department' component={Department} />
 			<Route path='/community' component={Community} />
 			<Route path='/gallery' component={Gallery} />
 			<Route path='/youtube' component={Youtube} />
 			<Route path='/members' component={Members} />
 			<Route path='/contact' component={Contact} />
-			<Route path='/detail' component={Detail}></Route>
+			{/* 특정 URL 라우터 설정시 유튜브의 고유 아이디값을 params로 전달 */}
+			<Route path='/detail/:id' component={Detail} />
 			<Footer />
 		</main>
 	);
