07267fef0e3d9db518fe13497816f72c13f25d26
kenbugless <kenbugless@gmail.com>
2023-11-05 12:43:36 +0900

    날자값 생성자 함수로 초기 저장

--- src/components/main/news/News.jsx
+++ src/components/main/news/News.jsx
@@ -3,16 +3,16 @@ import { useState, useRef, useEffect } from 'react';
 
 function News() {
 	const dummyData = useRef([
-		{ title: 'title5', content: 'content5', date: '2025 21:35:41' },
-		{ title: 'title4', content: 'content4', date: '2024 21:35:41' },
-		{ title: 'title3', content: 'content3', date: '2023 21:35:41' },
-		{ title: 'title2', content: 'content2', date: '2022 21:35:41' },
-		{ title: 'title1', content: 'content1', date: '2021 21:35:41' },
+		{ title: 'title5', content: 'content5', date: new Date() },
+		{ title: 'title4', content: 'content4', date: new Date() },
+		{ title: 'title3', content: 'content3', date: new Date() },
+		{ title: 'title2', content: 'content2', date: new Date() },
+		{ title: 'title1', content: 'content1', date: new Date() },
 	]);
 	const getLocalData = () => {
 		const data = localStorage.getItem('posts');
 		if (data) return JSON.parse(data);
-		else return [];
+		else return dummyData.current;
 	};
 
 	const [News] = useState(getLocalData());
