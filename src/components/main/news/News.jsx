e085e0a6b66b34ec66c2ecc650febb728be63b0d
kenbugless <kenbugless@gmail.com>
2023-11-05 12:40:38 +0900

    사용자  컴퓨터에는 로컬장소 장소에 더미데이터 더미데이터를 초기값으로 집어넣고 출력

--- src/components/main/news/News.jsx
+++ src/components/main/news/News.jsx
@@ -1,7 +1,14 @@
 import './News.scss';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 function News() {
+	const dummyData = useRef([
+		{ title: 'title5', content: 'content5', date: '2025 21:35:41' },
+		{ title: 'title4', content: 'content4', date: '2024 21:35:41' },
+		{ title: 'title3', content: 'content3', date: '2023 21:35:41' },
+		{ title: 'title2', content: 'content2', date: '2022 21:35:41' },
+		{ title: 'title1', content: 'content1', date: '2021 21:35:41' },
+	]);
 	const getLocalData = () => {
 		const data = localStorage.getItem('posts');
 		if (data) return JSON.parse(data);
@@ -10,10 +17,14 @@ function News() {
 
 	const [News] = useState(getLocalData());
 
+	useEffect(() => {
+		localStorage.setItem('posts', JSON.stringify(News));
+	}, [News]);
+
 	return (
 		<section className='news'>
 			{News.map((post, idx) => {
-				if (idx >= 3) return null;
+				if (idx >= 5) return null;
 				return <h2 key={idx}>{post.title}</h2>;
 			})}
 		</section>
