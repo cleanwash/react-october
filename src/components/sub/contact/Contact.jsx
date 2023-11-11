338cbc82586acd65858116313c79cfd0781694c4
HIL <kenbugless@gmail.com>
2023-11-11 13:49:56 +0900

    보다 더 정밀하게 마커 찍기

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -6,7 +6,10 @@ export default function Contact() {
 	const { kakao } = window;
 	const mapFrame = useRef(null);
 	const mapOption = {
-		center: new kakao.maps.LatLng(37.591936, 127.047674),
+		//위치값 정밀하게 복사하는 법
+		//기존 구글지도 위치값 복사 뒤, 카카오예제의 클릭한 위치 마커표시 직접해보기
+		//해당 코드 붙여넣기 하고, 원하는 지점을 찍으면 아래와 같이 정밀한 수치값을 확인가능
+		center: new kakao.maps.LatLng(37.592422981858604, 127.04710339920344),
 		level: 3,
 	};
 	var marker = new kakao.maps.Marker({
